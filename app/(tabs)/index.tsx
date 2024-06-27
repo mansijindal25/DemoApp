67b44ed0a1757e7d49c91e21acb8f0710fe11f65
mansijindal251 <mjindal@Veneklasen.com>
2024-06-27 17:42:21 +0530

    :lipstick: refactor: Fixation for the infinite loop, typo, links for the images used on the landing page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,56 +1,36 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+const PlaceholderImage = require("../../assets/images/adaptive-icon.png");
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
+    <View
+      style={{
+        flex: 1,
+        padding: 60,
+        backgroundColor: "#25292e",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
     >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hi! This is MJ</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <Text>
+        Hello World!!
+        <Text
+          style={{
+            color: "#fff",
+            fontWeight: 500,
+          }}
+        >
+          &nbsp;&nbsp;This is my first screen
+        </Text>
+      </Text>
+      <Image
+        source={PlaceholderImage}
+        style={{ width: 260, height: 480, borderRadius: 18, marginTop: 40 }}
+      ></Image>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
